9eddccca44764432aa71bc955de9d123f636fb79
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2025-05-07 15:56:45 +0700

    add report and record

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  projectId: 'i5thmw',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     experimentalStudio: true,
-  },
+    video: true,
+    screenshotOnRunFailure: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',        
+      overwrite: false,                    
+      html: true,                          
+      json: true                           
+    }
+  }
 };
-
